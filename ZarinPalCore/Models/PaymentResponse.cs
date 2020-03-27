bffccce1b2029dddeb6c248b8dd2fd99e6b9aca6
karim <appdomain.karim@gmail.com>
2020-03-27 19:38:36 +0430

    add .net core project

---
+++ ZarinPalCore/Models/PaymentResponse.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace ZarinPal
+{
+    public class PaymentResponse
+    {
+        public String Authority { set; get; }
+        public int Status { set; get; }
+        public String PaymentURL { set; get; }
+
+    }
+}
